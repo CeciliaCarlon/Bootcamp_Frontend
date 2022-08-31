3d08415b8e3a09ab68a36ad425a067a6cb76c548
Cecilia Carlon <ceciliacarlon2@gmail.com>
2022-08-31 10:10:05 -0300

    Se agrega el dark style

--- ManosALaObra3/src/app/app.module.ts
+++ ManosALaObra3/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {MatIconModule} from '@angular/material/icon';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +17,8 @@ import { AgregarTareaComponent } from './components/agregar-tarea/agregar-tarea.
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatIconModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
